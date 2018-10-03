717d08ca555f25319b617fcc3fc3c6bf1a9d78e7
chaim <chaimmw86@gmail.com>
2018-10-03 15:24:08 -0400

    add mailer service with Oauth2

--- src/services/index.js
+++ src/services/index.js
@@ -1,7 +1,10 @@
 const expenses = require('./expenses/expenses.service.js');
 const incomes = require('./incomes/incomes.service.js');
 const users = require('./users/users.service.js');
+const mailer = require('./mailer/mailer.service');
+
 module.exports = function (app) {
+  app.configure(mailer);
   app.configure(expenses);
   app.configure(incomes);
   app.configure(users);
---
+++ src/services/mailer/mailer.service.js
@@ -0,0 +1,89 @@
+const nodemailer = require('nodemailer');
+const { google } = require('googleapis');
+const OAuth2 = google.auth.OAuth2;
+
+
+
+class Mailer {
+
+  async create(data, params) {
+
+    // works with regular gmail but not secure
+    // const transporter = nodemailer.createTransport({
+    //   service: 'gmail',
+    //   auth: {
+    //     user: 'chaim.tester@gmail.com',
+    //     pass: 'partypooper'
+    //   },
+    //   tls: {
+    //     rejectUnauthorized: false
+    //   }
+    // });
+    // await transporter.sendMail(mailOptions, (err, info) => {
+    //   if (err) {
+    //     console.log('err');
+    //     console.error(err);
+    //   } else {
+    //     console.log('info', info);
+    //   }
+    // });
+
+    // more secure way of sending with gmail
+    // see: https://medium.com/@nickroach_50526/sending-emails-with-node-js-using-smtp-gmail-and-oauth2-316fe9c790a1
+    const mailOptions = {
+      from: 'chaim.tester@gmail.com',
+      to: 'chaimmw86@gmail.com',
+      subject: 'sent from Finance Tracker contact page',
+      generateTextFromHTML: true,
+      text: data,
+      html: data
+    };
+
+    const oauth2 = this.app.get('oauth2');
+
+    // set up oath2
+    const oauth2Client = new OAuth2(
+      oauth2.clientId,
+      oauth2.clientSecret,
+      'https://developers.google.com/oauthplayground' // Redirect URL
+    );
+
+    await oauth2Client.setCredentials({
+      refresh_token: oauth2.refreshToken
+    });
+
+    const tokens = await oauth2Client.getAccessToken();
+    // const accessToken = await tokens.credentials.access_token;
+
+    const smtpTransport = await nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        type: 'OAuth2',
+        user: this.app.get('email'),
+        clientId: oauth2.clientId,
+        clientSecret: oauth2.clientSecret,
+        refreshToken: oauth2.refreshToken,
+        accessToken: tokens
+      },
+      tls: {
+        rejectUnauthorized: false
+      }
+    });
+
+    await smtpTransport.sendMail(mailOptions, (error, response) => {
+      error ? console.log(error) : console.log(response);
+      smtpTransport.close();
+    });
+  }
+
+  setup(app){
+    this.app = app;
+  }
+}
+
+module.exports = function () {
+  const app = this;
+
+  app.use('/mailer', new Mailer());
+
+}
