993534308f776e87ecf7334214cd7fc5b9983657
chaim <chaimmw86@gmail.com>
2018-10-15 12:15:31 -0400

    add mongo production uri fix mailer add Procfile for heroku deploy

--- src/services/mailer/mailer.service.js
+++ src/services/mailer/mailer.service.js
@@ -6,7 +6,17 @@ const OAuth2 = google.auth.OAuth2;
 
 class Mailer {
 
-  async create(data, params) {
+  async create(data) {
+
+    // build message
+    const mailOptions = {
+      from: 'chaim.tester@gmail.com',
+      to: 'chaimmw86@gmail.com',
+      subject: 'sent from Finance Tracker contact page',
+      generateTextFromHTML: true,
+      text: data.message,
+      html: data.message
+    };
 
     // works with regular gmail but not secure
     // const transporter = nodemailer.createTransport({
@@ -30,18 +40,10 @@ class Mailer {
 
     // more secure way of sending with gmail
     // see: https://medium.com/@nickroach_50526/sending-emails-with-node-js-using-smtp-gmail-and-oauth2-316fe9c790a1
-    const mailOptions = {
-      from: 'chaim.tester@gmail.com',
-      to: 'chaimmw86@gmail.com',
-      subject: 'sent from Finance Tracker contact page',
-      generateTextFromHTML: true,
-      text: data,
-      html: data
-    };
 
     const oauth2 = this.app.get('oauth2');
 
-    // set up oath2
+    // configure oath2
     const oauth2Client = new OAuth2(
       oauth2.clientId,
       oauth2.clientSecret,
